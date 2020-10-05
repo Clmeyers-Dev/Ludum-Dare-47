2a11a8504f6e952f50252429568f606d6557ab8b
Clmeyers-Dev <70337853+Clmeyers-Dev@users.noreply.github.com>
2020-10-04 22:26:43 -0400

    spin

---
+++ OhBoyHereItIsGame/Assets/Spin.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spin : MonoBehaviour
+{
+    public float speedRotate;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        spin();
+    }
+    void spin()
+    {
+        transform.Rotate(Vector3.forward * speedRotate * Time.deltaTime);
+    }
+}
